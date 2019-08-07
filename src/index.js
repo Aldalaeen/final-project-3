7c3605fe263375effa7a692799f2726957c905e2
Aldalaeen <d.yousef71@gmail.com>
2019-08-07 02:35:20 -0700

    add

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
 <BrowserRouter>
